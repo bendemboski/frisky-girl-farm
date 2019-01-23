a934c772d055ed746a2d1cb28250c8b6d9f3c249
Ben Demboski <ben@turbopatent.com>
2019-01-22 22:06:16 -0800

    Fix tests

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.aws
 echo "decrypting credentials..."
 openssl aes-256-cbc -K $encrypted_bf8918a92040_key -iv $encrypted_bf8918a92040_iv -in ci/aws-credentials.enc -out ~/.aws/credentials -d
 echo "deploying..."
-yarn deploy
+yarn deploy production
