7db921ee47de890346817ca5ee6bfe6bbec5c936
Ben Demboski <demboskb@gmail.com>
2020-07-07 19:09:49 -0700

    Prevent concurrent set product API calls
    See code comment for details

--- app/services/order.js
+++ app/services/order.js
@@ -37,8 +37,13 @@ export default Service.extend({
     }
   }),
 
+  // The server doesn't have protections against concurrent API calls. This can
+  // be problematic if the same user makes concurrent calls, because it could
+  // result in creating two rows for them in the orders sheet. We could probably
+  // mitigate this on the server, but just preventing the client from making
+  // concurrent order API calls seems like it should be sufficient.
   setProductOrder: task(function*(product, quantity) {
     let products = yield this.api.setProductOrder(product.id, quantity);
     this.set('products', products);
-  })
+  }).enqueue()
 });
