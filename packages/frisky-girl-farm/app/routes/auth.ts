907f7fff5dda02a4a97474dca7f54fd750665246
Ben Demboski <demboskb@gmail.com>
2022-08-20 12:59:53 -0700

    Fix deprecation warning

--- packages/frisky-girl-farm/app/routes/auth.ts
+++ packages/frisky-girl-farm/app/routes/auth.ts
@@ -2,16 +2,18 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { taskFor } from 'ember-concurrency-ts';
 
+import type RouterService from '@ember/routing/router-service';
 import type UserService from '../services/user';
 import type OrderService from '../services/order';
 
 export default class AuthRoute extends Route {
+  @service('router') declare router: RouterService;
   @service declare user: UserService;
   @service declare order: OrderService;
 
   async redirect() {
     if (!(await taskFor(this.user.checkLoggedIn).perform())) {
-      this.transitionTo('login');
+      this.router.transitionTo('login');
     }
   }
 
